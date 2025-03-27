a08bd81d9341a011f41aa94bf69aa56f7d1bc7bf
chenjiezhu <cjzjolly@gmail.com>
2025-03-27 19:46:32 +0800

    数据库创建脚本更新

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,7 +1,26 @@
-CREATE TABLE `Counters` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `count` int(11) NOT NULL DEFAULT '1',
-  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+CREATE TABLE `Counters`
+(
+    `id`        int(11) NOT NULL AUTO_INCREMENT,
+    `count`     int(11) NOT NULL DEFAULT '1',
+    `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
+
+CREATE TABLE `RegInfo`
+(
+    `id`      int(11) NOT NULL AUTO_INCREMENT,
+    `company` VARCHAR(255) NOT NULL DEFAULT '',
+    `visitorName`  VARCHAR(255) NOT NULL DEFAULT '',
+    `visitorFromAddr`  VARCHAR(255) NOT NULL DEFAULT '',
+    `visitorTel`  VARCHAR(255) NOT NULL DEFAULT '',
+    `visitorCount`  VARCHAR(255) NOT NULL DEFAULT '',
+    `visitorCarID`  VARCHAR(255) NOT NULL DEFAULT '',
+    `visitorTime`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `note`  VARCHAR(255) NOT NULL DEFAULT '',
+    `visitorToAddr`  VARCHAR(255) NOT NULL DEFAULT '',
+    `visitorToApartment`  VARCHAR(255) NOT NULL DEFAULT '',
+    `visitorsReceptionistName`  VARCHAR(255) NOT NULL DEFAULT '',
+    `visitorsReceptionistTel`  VARCHAR(255) NOT NULL DEFAULT '',
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
