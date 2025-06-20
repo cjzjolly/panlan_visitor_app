ce587b49158eea3a0154eeaaaf99d22bbcbaf520
jiezhuchen <jiezhuchen@tencent.com>
2025-06-21 00:08:30 +0800

    增加保安端

--- src/main/resources/static/scan_result.js
+++ src/main/resources/static/scan_result.js
@@ -17,20 +17,21 @@
           const data = res.data;
           console.log("test test test:", data);
           // 填写表格
-          $('td:contains("访客信息") + td').text(data.visitorInfo || '');
-          $('td:contains("来访者") + td').text(data.visitorName || '');
-          $('td:contains("来访者公司名") + td').text(data.company || '');
-          $('td:contains("来访人地址") + td').text(data.visitorFromAddr || '');
-          $('td:contains("来访人联系方式") + td').text(data.visitorTel || '');
-          $('td:contains("来访人数") + td').text(data.visitorCount || '');
-          $('td:contains("车牌号码") + td').text(data.visitorCarID || '');
-          $('td:contains("预约来访时间") + td').text(data.visitorTime || '');
-          $('td:contains("进入时间") + td').text(data.visitorTime || '');
-          $('td:contains("到访地址") + td').text(data.visitorToAddr || '');
-          $('td:contains("部门名称") + td').text(data.visitorToApartment || '');
-          $('td:contains("接待人") + td').text(data.visitorsReceptionistName || '');
-          $('td:contains("接待人联系方式") + td').text(data.visitorsReceptionistTel || '');
-          $('td:contains("备注") + td').text(data.remark || '');
+          document.getElementsByClassName('visitorInfo')[0].textContent = data.visitorInfo || '';
+          document.getElementsByClassName('visitorName')[0].textContent = data.visitorName || '';
+          document.getElementsByClassName('company')[0].textContent = data.company || '';
+          document.getElementsByClassName('visitorFromAddr')[0].textContent = data.visitorFromAddr || '';
+          document.getElementsByClassName('visitorTel')[0].textContent = data.visitorTel || '';
+          document.getElementsByClassName('visitorCount')[0].textContent = data.visitorCount || '';
+          document.getElementsByClassName('visitorCarID')[0].textContent = data.visitorCarID || '';
+          document.getElementsByClassName('visitorTime')[0].textContent = data.visitorTime || '';
+          document.getElementsByClassName('enterTime')[0].textContent = data.enterTime || '';
+          document.getElementsByClassName('leaveTime')[0].textContent = data.leaveTime || '';
+          document.getElementsByClassName('visitorToAddr')[0].textContent = data.visitorToAddr || '';
+          document.getElementsByClassName('visitorToApartment')[0].textContent = data.visitorToApartment || '';
+          document.getElementsByClassName('visitorsReceptionistName')[0].textContent = data.visitorsReceptionistName || '';
+          document.getElementsByClassName('visitorsReceptionistTel')[0].textContent = data.visitorsReceptionistTel || '';
+          document.getElementsByClassName('remark')[0].textContent = data.remark || '';
         }
       });
     }
