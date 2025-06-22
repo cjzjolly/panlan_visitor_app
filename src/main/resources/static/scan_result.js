a32e5dd66372d330e0f918d344f69f29fd7b6c89
jiezhuchen <jiezhuchen@tencent.com>
2025-06-22 16:57:28 +0800

    增加保安端

--- src/main/resources/static/scan_result.js
+++ src/main/resources/static/scan_result.js
@@ -15,26 +15,26 @@
         }),
       }).done(function (res) {
         if (res && res.data !== undefined) {
-          const data = res.data;
+          const data = res.data[0];
           console.log("test test test:", data);
           // 填写表格
-          // document.getElementsByClassName('visitorInfo')[0].textContent = data.visitorInfo;
-          // document.getElementsByClassName('visitorName')[0].textContent = data.visitorName;
-          // document.getElementsByClassName('company')[0].textContent = data.company;
-          // document.getElementsByClassName('visitorFromAddr')[0].textContent = data.visitorFromAddr;
-          // document.getElementsByClassName('visitorTel')[0].textContent = data.visitorTel;
-          // document.getElementsByClassName('visitorCount')[0].textContent = data.visitorCount;
-          // document.getElementsByClassName('visitorCarID')[0].textContent = data.visitorCarID;
-          // document.getElementsByClassName('visitorTime')[0].textContent = data.visitorTime;
-          // document.getElementsByClassName('enterTime')[0].textContent = data.enterTime;
-          // document.getElementsByClassName('leaveTime')[0].textContent = data.leaveTime;
-          // document.getElementsByClassName('visitorToAddr')[0].textContent = data.visitorToAddr;
-          // document.getElementsByClassName('visitorToApartment')[0].textContent = data.visitorToApartment;
-          // document.getElementsByClassName('visitorsReceptionistName')[0].textContent = data.visitorsReceptionistName;
-          // document.getElementsByClassName('visitorsReceptionistTel')[0].textContent = data.visitorsReceptionistTel;
-          // document.getElementsByClassName('remark')[0].textContent = data.remark;
+          document.getElementsByClassName('visitorInfo')[0].textContent = data.visitorInfo;
+          document.getElementsByClassName('visitorName')[0].textContent = data.visitorName;
+          document.getElementsByClassName('company')[0].textContent = data.company;
+          document.getElementsByClassName('visitorFromAddr')[0].textContent = data.visitorFromAddr;
+          document.getElementsByClassName('visitorTel')[0].textContent = data.visitorTel;
+          document.getElementsByClassName('visitorCount')[0].textContent = data.visitorCount;
+          document.getElementsByClassName('visitorCarID')[0].textContent = data.visitorCarID;
+          document.getElementsByClassName('visitorTime')[0].textContent = data.visitorTime;
+          document.getElementsByClassName('enterTime')[0].textContent = data.enterTime;
+          document.getElementsByClassName('leaveTime')[0].textContent = data.leaveTime;
+          document.getElementsByClassName('visitorToAddr')[0].textContent = data.visitorToAddr;
+          document.getElementsByClassName('visitorToApartment')[0].textContent = data.visitorToApartment;
+          document.getElementsByClassName('visitorsReceptionistName')[0].textContent = data.visitorsReceptionistName;
+          document.getElementsByClassName('visitorsReceptionistTel')[0].textContent = data.visitorsReceptionistTel;
+          document.getElementsByClassName('remark')[0].textContent = data.remark;
           
-          document.querySelector('.visitorName').textContent = data.visitorName;
+          // document.querySelector('.visitorName').textContent = data.visitorName;
         
         }
       });
